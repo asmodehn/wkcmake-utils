99136d97db4938702c5d4797ff56bedadfc3a72f
AlexV <asmodehn@gmail.com>
2013-10-27 11:40:07 +0900

    fixed shell script to build on linux

--- tests/ProjectC/build.sh
+++ tests/ProjectC/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-BUILD_DIR="../ProjectC_build"
+BUILD_DIR="build"
 SRC_DIR=`pwd`
 BUILD_TYPE=Release
 ENABLE_TESTS=ON
 
 set +x
 
+ORI_DIR=`pwd`
+
 mkdir -p $BUILD_DIR && \
-pushd $BUILD_DIR
+cd $BUILD_DIR
 
 echo "Running in `pwd` :"
 cmake -DProjectC_BUILD_TYPE=$BUILD_TYPE -DProjectC_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
@@ -17,4 +19,4 @@ make
 
 ctest
 
-popd
+cd $ORI_DIR
